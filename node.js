4b9a4214ac3c5078b83fc3b08bf9081bf9003412
nizam-chenath <nizamstudymajliz@gmail.com>
2022-10-18 19:04:47 +0530

    data get method id done

--- node.js
+++ node.js
@@ -1,11 +1,25 @@
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
 
 http.createServer((req,res) => {
-    fs.readFile('index.html',(err,data) => {
-          res.writeHead(200,{'Content-Type' : "text/html"  })
-          res.write(data)
-          res.end()
-    })
+
+    var q = url.parse(req.url, true)
+    
+    if(q.pathname === "/"){
+        fs.readFile('index.html',(err,data) => {
+            res.writeHead(200,{'Content-Type' : "text/html"  })
+            res.write(data)
+            res.end()
+      })
+    }
+    else if(q.pathname === "/login"){
+        fs.readFile('login.html',(err,data) =>{
+            res.write(data)
+            res.end()
+        })
+    }else if(q.pathname === '/loginaction'){
+        console.log(q.query)
+    }
 
 }).listen(4000)
